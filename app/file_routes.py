862e8728bba9a0f1c5512e965c6e17727aee677d
Pandyaka Aptanagi <pandyaka.aptanagi@gmail.com>
2020-09-05 16:34:20 +0700

    change query name

--- app/file_routes.py
+++ app/file_routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import request
+from flask import request, jsonify
 
 
 @app.route('/file')
@@ -12,55 +12,55 @@ def index_file():
 
 @app.route('/encrypt/file/vigenere', methods=['GET'])
 def encrypt_file_vigenere():
-    query = request.args.get('query')
-    return query
+    query = request.args.get('plaintext')
+    return jsonify({'ciphertext':query})
 
 
 @app.route('/encrypt/file/vigenere/full', methods=['GET'])
 def encrypt_full_file_vigenere():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
 @app.route('/encrypt/file/vigenere/auto', methods=['GET'])
 def encrypt_auto_file_vigenere():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
 @app.route('/encrypt/file/vigenere/extended', methods=['GET'])
 def encrypt_extended_file_vigenere():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
 @app.route('/encrypt/file/playfair', methods=['GET'])
 def encrypt_file_playfair():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
 @app.route('/encrypt/file/super', methods=['GET'])
 def encrypt_file_super():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
 @app.route('/encrypt/file/affine', methods=['GET'])
 def encrypt_file_affine():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
 @app.route('/encrypt/file/hill', methods=['GET'])
 def encrypt_file_hill():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
 @app.route('/encrypt/file/enigma', methods=['GET'])
 def encrypt_file_enigma():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
@@ -69,53 +69,53 @@ def encrypt_file_enigma():
 
 @app.route('/decrypt/file/vigenere', methods=['GET'])
 def decrypt_file_vigenere():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/file/vigenere/full', methods=['GET'])
 def decrypt_full_file_vigenere():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/file/vigenere/auto', methods=['GET'])
 def decrypt_auto_file_vigenere():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/file/vigenere/extended', methods=['GET'])
 def decrypt_extended_file_vigenere():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/file/playfair', methods=['GET'])
 def decrypt_file_playfair():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/file/super', methods=['GET'])
 def decrypt_file_super():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/file/affine', methods=['GET'])
 def decrypt_file_affine():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/file/hill', methods=['GET'])
 def decrypt_file_hill():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/file/enigma', methods=['GET'])
 def decrypt_file_enigma():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
--- app/text_routes.py
+++ app/text_routes.py
@@ -12,55 +12,55 @@ def index():
 
 @app.route('/encrypt/text/vigenere', methods=['GET'])
 def encrypt_text_vigenere():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
 @app.route('/encrypt/text/vigenere/full', methods=['GET'])
 def encrypt_full_text_vigenere():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
 @app.route('/encrypt/text/vigenere/auto', methods=['GET'])
 def encrypt_auto_text_vigenere():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
 @app.route('/encrypt/text/vigenere/extended', methods=['GET'])
 def encrypt_extended_text_vigenere():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
 @app.route('/encrypt/text/playfair', methods=['GET'])
 def encrypt_text_playfair():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
 @app.route('/encrypt/text/super', methods=['GET'])
 def encrypt_text_super():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
 @app.route('/encrypt/text/affine', methods=['GET'])
 def encrypt_text_affine():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
 @app.route('/encrypt/text/hill', methods=['GET'])
 def encrypt_text_hill():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
 @app.route('/encrypt/text/enigma', methods=['GET'])
 def encrypt_text_enigma():
-    query = request.args.get('query')
+    query = request.args.get('plaintext')
     return query
 
 
@@ -69,53 +69,53 @@ def encrypt_text_enigma():
 
 @app.route('/decrypt/text/vigenere', methods=['GET'])
 def decrypt_text_vigenere():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/text/vigenere/full', methods=['GET'])
 def decrypt_full_text_vigenere():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/text/vigenere/auto', methods=['GET'])
 def decrypt_auto_text_vigenere():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/text/vigenere/extended', methods=['GET'])
 def decrypt_extended_text_vigenere():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/text/playfair', methods=['GET'])
 def decrypt_text_playfair():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/text/super', methods=['GET'])
 def decrypt_text_super():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/text/affine', methods=['GET'])
 def decrypt_text_affine():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/text/hill', methods=['GET'])
 def decrypt_text_hill():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
 
 
 @app.route('/decrypt/text/enigma', methods=['GET'])
 def decrypt_text_enigma():
-    query = request.args.get('query')
+    query = request.args.get('ciphertext')
     return query
