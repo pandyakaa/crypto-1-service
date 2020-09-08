c88d6790c987ad3d457dbf01d8935c3df450cf76
Pandyaka Aptanagi <pandyaka.aptanagi@gmail.com>
2020-09-09 01:00:26 +0700

    add full vigenere for file

--- app/file_routes.py
+++ app/file_routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import request, send_file, jsonify
-from .encrypter import standard_vigenere_encrypter, auto_key_vigenere_encrypter, extended_vigenere_encrypter, playfair_encrypter, super_encrypter, affine_encrypter, hill_encrypter
-from .decrypter import standard_vigenere_decrypter, auto_key_vigenere_decrypter, extended_vigenere_decrypter, playfair_decrypter, super_decrypter, affine_decrypter, hill_decrypter
+from .encrypter import standard_vigenere_encrypter, auto_key_vigenere_encrypter, extended_vigenere_encrypter, playfair_encrypter, super_encrypter, affine_encrypter, hill_encrypter, full_vigenere_encrypter
+from .decrypter import standard_vigenere_decrypter, auto_key_vigenere_decrypter, extended_vigenere_decrypter, playfair_decrypter, super_decrypter, affine_decrypter, hill_decrypter, full_vigenere_decrypter
 from .util.create_response import create_file_response
 from .util.handle_file import handle_file, handle_ascii_file
 
@@ -28,8 +28,14 @@ def encrypt_file_vigenere():
 
 @app.route('/encrypt/file/vigenere/full', methods=['GET', 'POST'])
 def encrypt_full_file_vigenere():
-    query = request.args.get('message')
-    return query
+    file = request.files['file']
+    key = request.form['key']
+    filename, file_context = handle_file(file)
+    encrypted_context = full_vigenere_encrypter(file_context, key)
+
+    complete_filename = create_file_response(
+        filename, encrypted_context)
+    return send_file(complete_filename)
 
 
 @app.route('/encrypt/file/vigenere/auto', methods=['GET', 'POST'])
@@ -132,8 +138,14 @@ def decrypt_file_vigenere():
 
 @app.route('/decrypt/file/vigenere/full', methods=['GET', 'POST'])
 def decrypt_full_file_vigenere():
-    query = request.args.get('message')
-    return query
+    file = request.files['file']
+    key = request.form['key']
+    filename, file_context = handle_file(file)
+    decrypted_context = full_vigenere_decrypter(file_context, key)
+
+    complete_filename = create_file_response(
+        filename, decrypted_context)
+    return send_file(complete_filename)
 
 
 @app.route('/decrypt/file/vigenere/auto', methods=['GET', 'POST'])
